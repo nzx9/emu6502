5fdb9e87d821ddeff13032334b0058604fa653d6
Navindu Dananga <navindudananga123@gmail.com>
2022-07-02 22:54:51 +0530

    trigger if methods added

--- src/flags.rs
+++ src/flags.rs
@@ -50,4 +50,60 @@ impl Flags {
             n: true,
         }
     }
+
+    pub fn trig_c_if(&mut self, condition: bool) {
+        if condition {
+            self.c = true;
+        } else {
+            self.c = false;
+        }
+    }
+
+    pub fn trig_z_if(&mut self, condition: bool) {
+        if condition {
+            self.z = true;
+        } else {
+            self.z = false;
+        }
+    }
+
+    pub fn trig_i_if(&mut self, condition: bool) {
+        if condition {
+            self.i = true;
+        } else {
+            self.i = false;
+        }
+    }
+
+    pub fn trig_d_if(&mut self, condition: bool) {
+        if condition {
+            self.d = true;
+        } else {
+            self.d = false;
+        }
+    }
+
+    pub fn trig_b_if(&mut self, condition: bool) {
+        if condition {
+            self.b = true;
+        } else {
+            self.b = false;
+        }
+    }
+
+    pub fn trig_v_if(&mut self, condition: bool) {
+        if condition {
+            self.v = true;
+        } else {
+            self.v = false;
+        }
+    }
+
+    pub fn trig_n_if(&mut self, condition: bool) {
+        if condition {
+            self.n = true;
+        } else {
+            self.n = false;
+        }
+    }
 }
